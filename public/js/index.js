703169df0d84c30f4029fb3f8b01db210ba82116
jwhester <jameshester88@gmail.com>
2019-12-01 19:09:54 -0600

    fixing issues with code

--- public/js/index.js
+++ public/js/index.js
@@ -4,19 +4,6 @@ const $ = require('jquery');
 
 // CAROUSEL
 $(document).ready(function(){
-    $('.carousel').carousel(
-        {
-            dist: 0,
-            padding: 0,
-            fullWidth: true,
-            indicators: true,
-            duration: 100,
-        }
-    );
+
 });
 
-autoplay()
-function autoplay() {
-    $('.carousel').carousel('next');
-    setTimeout(autoplay, 4500);
-}
